27a78a3c20be645465f5255744028beb9c3f5e9a
Armando Ivan Perez <ivan.perez.chan@hotmail.com>
2021-11-09 21:02:38 -0600

    r

--- routes/usuario.ts
+++ routes/usuario.ts
@@ -23,6 +23,16 @@ router.post('/', [
     validateField
 ], usuariosPost);
 
+router.post('/registroUsuario', [
+    // validarJWT,
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'El password debe ser mas de  6 letras').isLength({ min: 6 }),
+    check('correo', 'El correo no es valido').isEmail(),
+    check('correo').custom(existeEmail),
+    check('rol').custom(esRoleValido),
+    validateField
+], usuariosPost );
+
 router.put('/:id', [
     validarJWT,
     check('id', 'No es un ID valido').isMongoId(),
